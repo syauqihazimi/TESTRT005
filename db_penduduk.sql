0c011654f702ad51f280decb4c458fe22621744f
syauqihazimi <57092537+syauqihazimi@users.noreply.github.com>
2024-07-02 16:27:26 +0700

    Add files via upload
    database nya

---
+++ db_penduduk.sql
@@ -0,0 +1,308 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 21, 2024 at 09:44 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_penduduk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_anggota`
+--
+
+CREATE TABLE `tb_anggota` (
+  `id_anggota` int(11) NOT NULL,
+  `id_kk` int(11) NOT NULL,
+  `id_pend` int(11) NOT NULL,
+  `hubungan` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_datang`
+--
+
+CREATE TABLE `tb_datang` (
+  `id_datang` int(11) NOT NULL,
+  `nik` varchar(20) NOT NULL,
+  `nama_datang` varchar(20) NOT NULL,
+  `jekel` enum('LK','PR') NOT NULL,
+  `tgl_datang` date NOT NULL,
+  `pelapor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_kk`
+--
+
+CREATE TABLE `tb_kk` (
+  `id_kk` int(11) NOT NULL,
+  `no_kk` varchar(30) NOT NULL,
+  `kepala` varchar(20) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `desa` varchar(20) NOT NULL,
+  `rt` varchar(5) NOT NULL,
+  `rw` varchar(5) NOT NULL,
+  `kec` varchar(20) NOT NULL,
+  `kab` varchar(20) NOT NULL,
+  `prov` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_lahir`
+--
+
+CREATE TABLE `tb_lahir` (
+  `id_lahir` int(11) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `tgl_lh` date NOT NULL,
+  `jekel` enum('LK','PR') NOT NULL,
+  `id_kk` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_mendu`
+--
+
+CREATE TABLE `tb_mendu` (
+  `id_mendu` int(11) NOT NULL,
+  `id_pdd` int(11) NOT NULL,
+  `tgl_mendu` date NOT NULL,
+  `sebab` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pdd`
+--
+
+CREATE TABLE `tb_pdd` (
+  `id_pend` int(11) NOT NULL,
+  `nik` varchar(20) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `tempat_lh` varchar(15) NOT NULL,
+  `tgl_lh` date NOT NULL,
+  `jekel` enum('LK','PR') NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `desa` varchar(15) NOT NULL,
+  `rt` varchar(4) NOT NULL,
+  `rw` varchar(4) NOT NULL,
+  `agama` varchar(15) NOT NULL,
+  `kawin` varchar(15) NOT NULL,
+  `pekerjaan` varchar(30) NOT NULL,
+  `status` enum('Ada','Meninggal','Pindah') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pengguna`
+--
+
+CREATE TABLE `tb_pengguna` (
+  `id_pengguna` int(11) NOT NULL,
+  `nama_pengguna` varchar(20) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `level` enum('Administrator','Ketua RT','Sekretaris','Bendahara') NOT NULL,
+  `status` enum('aktif','tidak aktif') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tb_pengguna`
+--
+
+INSERT INTO `tb_pengguna` (`id_pengguna`, `nama_pengguna`, `username`, `password`, `level`, `status`) VALUES
+(1, 'Brodol', 'admin', 'admin', 'Administrator', 'aktif'),
+(3, 'Kang Emon', 'Sek1', 'Sek1', 'Sekretaris', 'aktif'),
+(10, 'Bendahara 1', 'Ben1', 'Ben1', 'Bendahara', 'aktif'),
+(11, 'RT01', 'RT01', 'RT01', 'Ketua RT', 'aktif'),
+(20, 'coba', 'coba', 'coba', 'Sekretaris', 'tidak aktif');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pindah`
+--
+
+CREATE TABLE `tb_pindah` (
+  `id_pindah` int(11) NOT NULL,
+  `id_pdd` int(11) NOT NULL,
+  `tgl_pindah` date NOT NULL,
+  `alasan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_anggota`
+--
+ALTER TABLE `tb_anggota`
+  ADD PRIMARY KEY (`id_anggota`),
+  ADD KEY `tb_anggota_ibfk_1` (`id_pend`),
+  ADD KEY `id_kk` (`id_kk`);
+
+--
+-- Indexes for table `tb_datang`
+--
+ALTER TABLE `tb_datang`
+  ADD PRIMARY KEY (`id_datang`),
+  ADD KEY `pelapor` (`pelapor`);
+
+--
+-- Indexes for table `tb_kk`
+--
+ALTER TABLE `tb_kk`
+  ADD PRIMARY KEY (`id_kk`);
+
+--
+-- Indexes for table `tb_lahir`
+--
+ALTER TABLE `tb_lahir`
+  ADD PRIMARY KEY (`id_lahir`),
+  ADD KEY `id_kk` (`id_kk`);
+
+--
+-- Indexes for table `tb_mendu`
+--
+ALTER TABLE `tb_mendu`
+  ADD PRIMARY KEY (`id_mendu`),
+  ADD KEY `id_pdd` (`id_pdd`);
+
+--
+-- Indexes for table `tb_pdd`
+--
+ALTER TABLE `tb_pdd`
+  ADD PRIMARY KEY (`id_pend`);
+
+--
+-- Indexes for table `tb_pengguna`
+--
+ALTER TABLE `tb_pengguna`
+  ADD PRIMARY KEY (`id_pengguna`);
+
+--
+-- Indexes for table `tb_pindah`
+--
+ALTER TABLE `tb_pindah`
+  ADD PRIMARY KEY (`id_pindah`),
+  ADD KEY `id_pdd` (`id_pdd`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_anggota`
+--
+ALTER TABLE `tb_anggota`
+  MODIFY `id_anggota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `tb_datang`
+--
+ALTER TABLE `tb_datang`
+  MODIFY `id_datang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tb_kk`
+--
+ALTER TABLE `tb_kk`
+  MODIFY `id_kk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `tb_lahir`
+--
+ALTER TABLE `tb_lahir`
+  MODIFY `id_lahir` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `tb_mendu`
+--
+ALTER TABLE `tb_mendu`
+  MODIFY `id_mendu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tb_pdd`
+--
+ALTER TABLE `tb_pdd`
+  MODIFY `id_pend` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `tb_pengguna`
+--
+ALTER TABLE `tb_pengguna`
+  MODIFY `id_pengguna` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `tb_pindah`
+--
+ALTER TABLE `tb_pindah`
+  MODIFY `id_pindah` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_anggota`
+--
+ALTER TABLE `tb_anggota`
+  ADD CONSTRAINT `tb_anggota_ibfk_1` FOREIGN KEY (`id_pend`) REFERENCES `tb_pdd` (`id_pend`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tb_anggota_ibfk_2` FOREIGN KEY (`id_kk`) REFERENCES `tb_kk` (`id_kk`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tb_datang`
+--
+ALTER TABLE `tb_datang`
+  ADD CONSTRAINT `tb_datang_ibfk_1` FOREIGN KEY (`pelapor`) REFERENCES `tb_pdd` (`id_pend`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tb_lahir`
+--
+ALTER TABLE `tb_lahir`
+  ADD CONSTRAINT `tb_lahir_ibfk_1` FOREIGN KEY (`id_kk`) REFERENCES `tb_kk` (`id_kk`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tb_mendu`
+--
+ALTER TABLE `tb_mendu`
+  ADD CONSTRAINT `tb_mendu_ibfk_1` FOREIGN KEY (`id_pdd`) REFERENCES `tb_pdd` (`id_pend`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tb_pindah`
+--
+ALTER TABLE `tb_pindah`
+  ADD CONSTRAINT `tb_pindah_ibfk_1` FOREIGN KEY (`id_pdd`) REFERENCES `tb_pdd` (`id_pend`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
